bfe8fc4354f29e835e20fc7cf3b72ce1504ea0bd
Mohanad Abu Nayla <aljaboori@gmail.com>
2020-10-09 11:24:32 +0800

    Country code picker implementation.

--- app/src/main/java/com/abunayla/wattson/ui/fragments/PowerCostFragment.kt
+++ app/src/main/java/com/abunayla/wattson/ui/fragments/PowerCostFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.abunayla.wattson.R
 import com.abunayla.wattson.viewmodel.PowerCostViewModel
+import com.hbb20.CountryCodePicker
 import kotlinx.android.synthetic.main.fragment_power_cost.*
 
 class PowerCostFragment : Fragment() {
@@ -20,14 +22,28 @@ class PowerCostFragment : Fragment() {
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_power_cost, container, false)
-
+        val countryPicker = view.findViewById<CountryCodePicker>(R.id.countryPicker)
+        var currentSelection: String = countryPicker.selectedCountryNameCode
         viewModel = ViewModelProvider(this).get(PowerCostViewModel::class.java)
 
-        viewModel.readPowerCost("IT").observe(viewLifecycleOwner, Observer {
-            tvShowCost.text = it.first().cost.toString()
-            tvShowCurrency.text = it.first().currency
-            tvshowIsoCode.text = it.first().iso_code
-        })
+        viewModel.readPowerCost(currentSelection).observe(
+            viewLifecycleOwner, Observer {
+                tvShowCost.text = it.first().cost.toString()
+                tvShowCurrency.text = it.first().currency
+                tvshowIsoCode.text = it.first().iso_code
+            })
+
+
+        countryPicker.setOnCountryChangeListener {
+            currentSelection = countryPicker.selectedCountryNameCode
+            viewModel.readPowerCost(currentSelection).observe(
+                viewLifecycleOwner, Observer {
+                    tvShowCost.text = it.first().cost.toString()
+                    tvShowCurrency.text = it.first().currency
+                    tvshowIsoCode.text = it.first().iso_code
+                })
+
+        }
 
         return view
     }
