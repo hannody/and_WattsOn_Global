e228f38ca7af6bdf0262aa2b3b7f97b884849959
Mohanad Abu Nayla <aljaboori@gmail.com>
2020-10-20 05:05:08 +0800

    Update: Timer for editText watts input.

--- app/src/main/java/com/abunayla/wattson/ui/fragments/PowerCostFragment.kt
+++ app/src/main/java/com/abunayla/wattson/ui/fragments/PowerCostFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,17 +16,20 @@ import com.hbb20.CountryCodePicker
 import com.sdsmdg.harjot.crollerTest.Croller
 import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener
 import kotlinx.android.synthetic.main.fragment_power_cost.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class PowerCostFragment : Fragment() {
     private lateinit var viewModel: PowerCostViewModel
-    private lateinit var sbProgressText: String
-    private lateinit var hCostTxt: String
+    private var sbProgressText: String = "1"
+    private var hCostTxt: String = "NA"
     private var cost: Double = 0.toDouble()
     private var currency: String = ""
     private var isoCode: String = ""
     private var watts: Int = 0
     private var hoursPerDay = 1
+    private lateinit var timer: Timer
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,7 +44,6 @@ class PowerCostFragment : Fragment() {
         sbProgressText = getString(R.string.str_seekBar_prog_txt)
         hCostTxt = getString(R.string.str_h_cost)
         viewModel = ViewModelProvider(this).get(PowerCostViewModel::class.java)
-
 
 
         countryPicker.setOnCountryChangeListener {
@@ -61,11 +64,22 @@ class PowerCostFragment : Fragment() {
             override fun onStopTrackingTouch(croller: Croller?) {}
         })
 
+ //Watts input (editText events)
+        timer = Timer()
+        wattsInput.doAfterTextChanged {
+            if (wattsInput.text.isNotEmpty()) {
+                timer.cancel()
+                timer = Timer()
+                //watts = wattsInput.text.toString().toInt()
+                timer.schedule(1500) {
+                    Log.i("TAG", "Watts INPUT EVENET!")
+                    watts = wattsInput.text.toString().toInt()
+                }
+            }else{
+                timer.cancel()
+            }
 
-        wattsInput.setOnClickListener {
-            watts = wattsInput.text.toString().toInt()
         }
-
         // Inflate the layout for this fragment
         return view
     }
