0c3bb630403ef051b992220ef0f3295d2fa99633
Mohanad Abu Nayla <aljaboori@gmail.com>
2020-11-15 17:54:36 +0800

    Update: Added info btn for the action bar.

--- app/src/main/java/com/abunayla/wattson/ui/MainActivity.kt
+++ app/src/main/java/com/abunayla/wattson/ui/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.abunayla.wattson.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.abunayla.wattson.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.about_me, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "HELLO", Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
     }
 }
