eadcb5b8444346824a0c6ede161e5d880ed104b7
Mohanad Abu Nayla <aljaboori@gmail.com>
2020-11-18 12:59:38 +0800

    Update: Splash screen.

--- app/src/main/java/com/abunayla/wattson/ui/MainActivity.kt
+++ app/src/main/java/com/abunayla/wattson/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import com.abunayla.wattson.ui.dialogs.AboutMeDialogFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
 
